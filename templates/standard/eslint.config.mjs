c2d658cf0e89ca56e1349a1f3080474fabb66730
Michael Harrison <michael.harrison@bjss.com>
2025-10-15 17:55:54 +0100

    upgrade eslint in standard template

---
+++ templates/standard/eslint.config.mjs
@@ -0,0 +1,26 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default defineConfig(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+  {
+    files: ["**/*.test.ts"],
+    languageOptions: {
+      globals: {
+        ...globals.jest,
+      },
+    },
+  }
+);
